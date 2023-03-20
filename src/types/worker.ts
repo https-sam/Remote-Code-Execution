479f0add112f9a3eac3dccc539f8ab4c9bef34f3
Sam Goto <osgo1115@icloud.com>
2023-03-19 21:45:54 -0600

    refactor: code refactor

--- src/types/worker.ts
+++ src/types/worker.ts
@@ -1,29 +1,29 @@
 export type language = "python3" | "javascript";
 export type fileFormat = "py" | "js";
-export type Stdout = string
+export type Stdout = string;
 
 export interface CodeContext {
 	/**
 	 * raw string code to be excuted
 	 */
-	code: string
+	code: string;
 
 	/**
 	 * A method name in class to be executed
 	 */
-	functionName: string
+	functionName: string;
 }
 
 interface BaseError {
 	/**
 	 * Indicates an error during the process of spinning up the container
 	 */
-	error: boolean
+	error: boolean;
 
 	/**
 	 * An error message in case the job fails
 	 */
-	errorMessage?: string
+	errorMessage?: string;
 }
 
 export interface ContainerInitialization extends BaseError {
@@ -39,16 +39,13 @@ export interface WriteFileStatus {
 	 */
 	filePath: string;
 
-
 	/**
 	 * The file format of the created file
 	 */
 	fileFormat: string;
 }
 
-
-
-export interface JobStatus extends BaseError{
+export interface JobStatus extends BaseError {
 	/**
 	 * A brief message that describes that job status
 	 */
@@ -65,10 +62,9 @@ export interface JobStatus extends BaseError{
 	filePath?: string;
 }
 
-export interface ExecuteContainer extends BaseError{
+export interface ExecuteContainer extends BaseError {
 	/**
 	 * stdout from the container as a result of running the code
 	 */
-	codeOutput?: Stdout
-
+	codeOutput?: Stdout;
 }
