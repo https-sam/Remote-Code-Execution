13dda331975af841a0197528f3c8d5370e51f3a4
Sam Goto <osgo1115@icloud.com>
2023-03-19 22:49:27 -0600

    indentation

--- src/types/worker.ts
+++ src/types/worker.ts
@@ -3,68 +3,68 @@ export type fileFormat = "py" | "js";
 export type Stdout = string;
 
 export interface CodeContext {
-	/**
-	 * raw string code to be excuted
-	 */
-	code: string;
+  /**
+   * raw string code to be excuted
+   */
+  code: string;
 
-	/**
-	 * A method name in class to be executed
-	 */
-	functionName: string;
+  /**
+   * A method name in class to be executed
+   */
+  functionName: string;
 }
 
 interface BaseError {
-	/**
-	 * Indicates an error during the process of spinning up the container
-	 */
-	error: boolean;
+  /**
+   * Indicates an error during the process of spinning up the container
+   */
+  error: boolean;
 
-	/**
-	 * An error message in case the job fails
-	 */
-	errorMessage?: string;
+  /**
+   * An error message in case the job fails
+   */
+  errorMessage?: string;
 }
 
 export interface ContainerInitialization extends BaseError {
-	/**
-	 * The ID of the newly created container
-	 */
-	containerID: string;
+  /**
+   * The ID of the newly created container
+   */
+  containerID: string;
 }
 
 export interface WriteFileStatus {
-	/**
-	 * Newly created temp file that contain the context if exists
-	 */
-	filePath: string;
+  /**
+   * Newly created temp file that contain the context if exists
+   */
+  filePath: string;
 
-	/**
-	 * The file format of the created file
-	 */
-	fileFormat: string;
+  /**
+   * The file format of the created file
+   */
+  fileFormat: string;
 }
 
 export interface JobStatus extends BaseError {
-	/**
-	 * A brief message that describes that job status
-	 */
-	message: string;
+  /**
+   * A brief message that describes that job status
+   */
+  message: string;
 
-	/**
-	 * indicates if the job is retryable
-	 */
-	retryable: boolean;
+  /**
+   * indicates if the job is retryable
+   */
+  retryable: boolean;
 
-	/**
-	 * Newly created temp file that contain the context if exists
-	 */
-	filePath?: string;
+  /**
+   * Newly created temp file that contain the context if exists
+   */
+  filePath?: string;
 }
 
 export interface ExecuteContainer extends BaseError {
-	/**
-	 * stdout from the container as a result of running the code
-	 */
-	codeOutput?: Stdout;
+  /**
+   * stdout from the container as a result of running the code
+   */
+  codeOutput?: Stdout;
 }
--- src/worker.ts
+++ src/worker.ts
@@ -3,260 +3,260 @@ import crypto from "crypto";
 import fs from "fs";
 import path from "path";
 import {
-	CodeContext,
-	ContainerInitialization,
-	JobStatus,
-	WriteFileStatus,
-	language,
-	fileFormat,
-	ExecuteContainer,
-	Stdout,
+  CodeContext,
+  ContainerInitialization,
+  JobStatus,
+  WriteFileStatus,
+  language,
+  fileFormat,
+  ExecuteContainer,
+  Stdout,
 } from "./types/worker";
 import { containerCPULimit, containerMemLimit, mainClassName } from "./config";
 
 const fileFormats: Record<language, fileFormat> = {
-	python3: "py",
-	javascript: "js",
+  python3: "py",
+  javascript: "js",
 };
 
 class JobWorker {
-	public language: language;
-	public codeContext: CodeContext;
-	public filename: string;
-	public containerID: string;
+  public language: language;
+  public codeContext: CodeContext;
+  public filename: string;
+  public containerID: string;
 
-	constructor(language: language, codeContext: CodeContext) {
-		this.language = language;
-		this.codeContext = codeContext;
-		this.filename = "";
-		this.containerID = "";
-	}
+  constructor(language: language, codeContext: CodeContext) {
+    this.language = language;
+    this.codeContext = codeContext;
+    this.filename = "";
+    this.containerID = "";
+  }
 
-	/**
-	 * Creates an appropriate docker container
-	 * to execute the target code
-	 */
-	createContainer(): Promise<ContainerInitialization> {
-		return new Promise((resolve, reject) => {
-			const initCommand = `docker create ${containerMemLimit} ${containerCPULimit} ${this.language}`;
-			if (!(this.language in fileFormats)) {
-				return reject({
-					error: true,
-					errorMessage: "Invalid container name.",
-				});
-			}
+  /**
+   * Creates an appropriate docker container
+   * to execute the target code
+   */
+  createContainer(): Promise<ContainerInitialization> {
+    return new Promise((resolve, reject) => {
+      const initCommand = `docker create ${containerMemLimit} ${containerCPULimit} ${this.language}`;
+      if (!(this.language in fileFormats)) {
+        return reject({
+          error: true,
+          errorMessage: "Invalid container name.",
+        });
+      }
 
-			child.exec(initCommand, (error, containerID, stderr) => {
-				if (error) {
-					reject({
-						error: true,
-						errorMessage: error.message,
-					});
-				} else if (stderr) {
-					reject({
-						error: true,
-						errorMessage: stderr,
-					});
-				} else {
-					this.containerID = containerID.trim();
-					resolve({
-						error: false,
-						containerID: this.containerID,
-					});
-				}
-			});
-		});
-	}
+      child.exec(initCommand, (error, containerID, stderr) => {
+        if (error) {
+          reject({
+            error: true,
+            errorMessage: error.message,
+          });
+        } else if (stderr) {
+          reject({
+            error: true,
+            errorMessage: stderr,
+          });
+        } else {
+          this.containerID = containerID.trim();
+          resolve({
+            error: false,
+            containerID: this.containerID,
+          });
+        }
+      });
+    });
+  }
 
-	/**
-	 * Returns a piece of code that executes the class method
-	 * this is language specific, so defined using switch cases
-	 */
-	transformCodeIntoExecutable(): string {
-		switch (this.language) {
-			case "python3": {
-				return `\nprint(${mainClassName}().${this.codeContext.functionName}())`;
-			}
-			default:
-				return "";
-		}
-	}
+  /**
+   * Returns a piece of code that executes the class method
+   * this is language specific, so defined using switch cases
+   */
+  transformCodeIntoExecutable(): string {
+    switch (this.language) {
+      case "python3": {
+        return `\nprint(${mainClassName}().${this.codeContext.functionName}())`;
+      }
+      default:
+        return "";
+    }
+  }
 
-	/**
-	 * writes a code into a temp file
-	 */
-	private async writeFile(): Promise<WriteFileStatus> {
-		return new Promise((resolve, reject) => {
-			const fileFormat: string = fileFormats[this.language];
-			this.filename = `${crypto.randomBytes(32).toString("hex")}.${fileFormat}`;
-			const filePath = path.join(__dirname, "..", "temp", `${this.filename}`);
+  /**
+   * writes a code into a temp file
+   */
+  private async writeFile(): Promise<WriteFileStatus> {
+    return new Promise((resolve, reject) => {
+      const fileFormat: string = fileFormats[this.language];
+      this.filename = `${crypto.randomBytes(32).toString("hex")}.${fileFormat}`;
+      const filePath = path.join(__dirname, "..", "temp", `${this.filename}`);
 
-			// appending a line to execute a specific function
-			this.codeContext.code += this.transformCodeIntoExecutable();
+      // appending a line to execute a specific function
+      this.codeContext.code += this.transformCodeIntoExecutable();
 
-			fs.writeFile(filePath, this.codeContext.code, (error) => {
-				if (error) {
-					reject(error.message);
-				} else {
-					resolve({ filePath, fileFormat });
-				}
-			});
-		});
-	}
+      fs.writeFile(filePath, this.codeContext.code, (error) => {
+        if (error) {
+          reject(error.message);
+        } else {
+          resolve({ filePath, fileFormat });
+        }
+      });
+    });
+  }
 
-	/**
-	 * copies the target code into the newly created
-	 * isolated container
-	 */
-	copyContext(): Promise<string> {
-		return new Promise(async (resolve, reject) => {
-			if (!this.containerID) {
-				reject("ContainerID not found.");
-			}
-			this.writeFile()
-				.then(({ filePath, fileFormat }) => {
-					const initCommand = `docker cp ${filePath} ${this.containerID}:/src/target.${fileFormat}`;
-					child.exec(initCommand, (error, containerID, stderr) => {
-						if (error) {
-							reject(error.message);
-						} else if (stderr) {
-							reject(stderr);
-						} else {
-							resolve(containerID.trim());
-						}
-					});
-				})
-				.catch((e) => {
-					reject(e);
-				});
-		});
-	}
+  /**
+   * copies the target code into the newly created
+   * isolated container
+   */
+  copyContext(): Promise<string> {
+    return new Promise(async (resolve, reject) => {
+      if (!this.containerID) {
+        reject("ContainerID not found.");
+      }
+      this.writeFile()
+        .then(({ filePath, fileFormat }) => {
+          const initCommand = `docker cp ${filePath} ${this.containerID}:/src/target.${fileFormat}`;
+          child.exec(initCommand, (error, containerID, stderr) => {
+            if (error) {
+              reject(error.message);
+            } else if (stderr) {
+              reject(stderr);
+            } else {
+              resolve(containerID.trim());
+            }
+          });
+        })
+        .catch((e) => {
+          reject(e);
+        });
+    });
+  }
 
-	/**
-	 *  Excecutes phase 1
-	 *  1] Creates a new container
-	 *  2] Copies the code into the contaienr
-	 */
-	initContainer(): Promise<JobStatus> {
-		return new Promise(async (resolve, reject) => {
-			this.createContainer()
-				.then(async ({ error, errorMessage }) => {
-					if (error) return new Error(errorMessage);
-					return this.copyContext()
-						.then(() => {
-							resolve({
-								message: `Job has succedded.`,
-								error: false,
-								retryable: true,
-							});
-						})
-						.catch((e) => {
-							throw new Error(e);
-						});
-				})
-				.catch((e) => {
-					reject({
-						message: `Job has failed for the following reason(s): ${e}.`,
-						jobFailed: true,
-						retryable: true,
-					});
-				});
-		});
-	}
+  /**
+   *  Excecutes phase 1
+   *  1] Creates a new container
+   *  2] Copies the code into the contaienr
+   */
+  initContainer(): Promise<JobStatus> {
+    return new Promise(async (resolve, reject) => {
+      this.createContainer()
+        .then(async ({ error, errorMessage }) => {
+          if (error) return new Error(errorMessage);
+          return this.copyContext()
+            .then(() => {
+              resolve({
+                message: `Job has succedded.`,
+                error: false,
+                retryable: true,
+              });
+            })
+            .catch((e) => {
+              throw new Error(e);
+            });
+        })
+        .catch((e) => {
+          reject({
+            message: `Job has failed for the following reason(s): ${e}.`,
+            jobFailed: true,
+            retryable: true,
+          });
+        });
+    });
+  }
 
-	/**
-	 * Executes phase 2
-	 * 1] Spin up the container
-	 * 2] Record the output from the container
-	 */
-	startContainer(): Promise<ExecuteContainer> {
-		return new Promise((resolve, reject) => {
-			this.initContainer()
-				.then((jobStatus: JobStatus) => {
-					if (!jobStatus.error && this.containerID) {
-						const startContainer = `docker start -a ${this.containerID}`;
-						child.exec(startContainer, (error, stdout, stderr) => {
-							if (error) {
-								reject({
-									error: true,
-									errorMessage: error.message,
-								} as ExecuteContainer);
-							} else if (stderr) {
-								reject({
-									error: true,
-									errorMessage: stderr,
-								} as ExecuteContainer);
-							} else {
-								resolve({
-									error: false,
-									codeOutput: stdout.trim(),
-								} as ExecuteContainer);
-							}
-						});
-					} else {
-						// job failed
-						reject({
-							error: true,
-							errorMessage: `Job has failed in the process of initializing the container for the following reason(s): ${jobStatus.message}`,
-						} as ExecuteContainer);
-					}
-				})
-				.catch((_) => {});
-		});
-	}
+  /**
+   * Executes phase 2
+   * 1] Spin up the container
+   * 2] Record the output from the container
+   */
+  startContainer(): Promise<ExecuteContainer> {
+    return new Promise((resolve, reject) => {
+      this.initContainer()
+        .then((jobStatus: JobStatus) => {
+          if (!jobStatus.error && this.containerID) {
+            const startContainer = `docker start -a ${this.containerID}`;
+            child.exec(startContainer, (error, stdout, stderr) => {
+              if (error) {
+                reject({
+                  error: true,
+                  errorMessage: error.message,
+                } as ExecuteContainer);
+              } else if (stderr) {
+                reject({
+                  error: true,
+                  errorMessage: stderr,
+                } as ExecuteContainer);
+              } else {
+                resolve({
+                  error: false,
+                  codeOutput: stdout.trim(),
+                } as ExecuteContainer);
+              }
+            });
+          } else {
+            // job failed
+            reject({
+              error: true,
+              errorMessage: `Job has failed in the process of initializing the container for the following reason(s): ${jobStatus.message}`,
+            } as ExecuteContainer);
+          }
+        })
+        .catch((_) => {});
+    });
+  }
 
-	/**
-	 * Removes a container with the provided containerID
-	 */
-	async removeContainer(): Promise<Stdout> {
-		return new Promise((resolve, reject) => {
-			if (!this.containerID) {
-				reject("ContainerID not found.");
-			}
-			const removeContainer = `docker rm --force ${this.containerID}`;
-			child.exec(removeContainer, (error, stdout, stderr) => {
-				if (error) {
-					reject(error.message);
-				} else if (stderr) {
-					reject(stderr);
-				} else {
-					resolve(stdout);
-				}
-			});
-		});
-	}
+  /**
+   * Removes a container with the provided containerID
+   */
+  async removeContainer(): Promise<Stdout> {
+    return new Promise((resolve, reject) => {
+      if (!this.containerID) {
+        reject("ContainerID not found.");
+      }
+      const removeContainer = `docker rm --force ${this.containerID}`;
+      child.exec(removeContainer, (error, stdout, stderr) => {
+        if (error) {
+          reject(error.message);
+        } else if (stderr) {
+          reject(stderr);
+        } else {
+          resolve(stdout);
+        }
+      });
+    });
+  }
 
-	/**
-	 * Deletes the temp file in /temp folder
-	 */
-	async removeCacheFile(): Promise<Stdout> {
-		return new Promise((resolve, reject) => {
-			if (!this.filename) {
-				return reject("Filename not found.");
-			}
-			const removeContainer = `rm  ${__dirname}/../temp/${this.filename}`;
-			child.exec(removeContainer, (error, stdout, stderr) => {
-				if (error) {
-					reject(error.message);
-				} else if (stderr) {
-					reject(stderr);
-				} else {
-					resolve(stdout);
-				}
-			});
-		});
-	}
+  /**
+   * Deletes the temp file in /temp folder
+   */
+  async removeCacheFile(): Promise<Stdout> {
+    return new Promise((resolve, reject) => {
+      if (!this.filename) {
+        return reject("Filename not found.");
+      }
+      const removeContainer = `rm  ${__dirname}/../temp/${this.filename}`;
+      child.exec(removeContainer, (error, stdout, stderr) => {
+        if (error) {
+          reject(error.message);
+        } else if (stderr) {
+          reject(stderr);
+        } else {
+          resolve(stdout);
+        }
+      });
+    });
+  }
 
-	/**
-	 * Cleans up the job
-	 * Destroys the docker container & deletes cache files
-	 */
-	async cleanupJob(): Promise<any> {
-		const jobs: Promise<any>[] = [];
-		jobs.push(this.removeContainer());
-		jobs.push(this.removeCacheFile());
-		return Promise.all(jobs);
-	}
+  /**
+   * Cleans up the job
+   * Destroys the docker container & deletes cache files
+   */
+  async cleanupJob(): Promise<any> {
+    const jobs: Promise<any>[] = [];
+    jobs.push(this.removeContainer());
+    jobs.push(this.removeCacheFile());
+    return Promise.all(jobs);
+  }
 }
 
 export default JobWorker;
